e8953122a15534b24e3647d9c220343571f72859
Arivaldo do Nascimento Silva <arivaldon.silva@gmail.com>
2018-11-06 15:49:31 -0200

    Primeiro commit - projeto criado

---
+++ src/main/java/com/nelioalves/workshopmongo/Workshopmongo2Application.java
@@ -0,0 +1,12 @@
+package com.nelioalves.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Workshopmongo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Workshopmongo2Application.class, args);
+	}
+}
